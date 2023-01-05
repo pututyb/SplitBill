123b491d72eba865688f3b2326648efd793612e0
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-01-06 00:26:36 +0700

    Initial Commit

---
+++ WeSplitTraining4/WeSplitTraining4App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitTraining4App.swift
+//  WeSplitTraining4
+//
+//  Created by Putut Yusri Bahtiar on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitTraining4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
