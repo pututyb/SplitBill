b50b124e8046cc31d232f0bac92c65eb16f002cc
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-01-12 13:07:48 +0700

    commit

--- WeSplitTraining4/ContentView.swift
+++ WeSplitTraining4/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount = 0
+    @State private var numberOfPeople = 2
+    @State private var tipsPercentage = 0
+    @State private var currency = "USD"
+    
+    let tipsPercentages = [10, 15, 20, 25]
+    let currencies = ["USD", "EUR", "IDR", "JPY"]
+    
+    
+    var totalPerson: Int {
+        let checkAmount = Int(amount)
+        let checkNumberOfPeople = Int(numberOfPeople + 2)
+        
+        let totalTips = checkAmount / 100 * tipsPercentage
+        let grandTotal = totalTips + amount
+        let payPerPerson = grandTotal / checkNumberOfPeople
+        
+        return payPerPerson
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $amount, format: .currency(code: currency))
+                        .id(currency)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Currency", selection: $currency) {
+                        ForEach(currencies, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                Section {
+                    Picker("Tips Percentage", selection: $tipsPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                } header: {
+                    Text("How Much Tips ?")
+                }
+                
+                Section {
+                    Text(amount, format: .currency(code: currency))
+                } header: {
+                    Text("Total Amount")
+                }
+                
+                Section {
+                    Text(totalPerson, format: .currency(code: currency))
+                } header: {
+                    Text("Amount per rperson")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
